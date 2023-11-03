e39b5e7f9cabf2266ba0f9c7c9640919243dfc67
deprecated-guy <rammleid123@gmail.com>
2023-11-03 20:34:02 +0100

    fix(ui): * fix inputs behavior in blur event

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
 	{
-		path: '',
-		loadComponent: () => import('./component/welcome-page/welcome-page.component').then((c) => c.WelcomePageComponent),
+		path: '**',
+		loadComponent: () => import('src/shared/pages/not-found/not-found.component').then((r) => r.NotFoundComponent),
 	},
 ];
